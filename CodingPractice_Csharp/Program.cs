545b8e2aeb30e461a220f77a34b66ce8334b5d0d
selfstudymaster <webselfmaster@gmail.com>
2023-01-22 13:55:35 +0900

    first commit

---
+++ CodingPractice_Csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodingPractice_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
